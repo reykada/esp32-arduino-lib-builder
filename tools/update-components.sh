7a721ddf3c6a271066659731ee81b726d5913e30
Andrey <andrey.gulyants@gmail.com>
2025-02-21 23:28:26 +0000

    Replaced repositories links

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -6,7 +6,7 @@ source ./tools/config.sh
 # CLONE/UPDATE TINYUSB
 #
 echo "Updating TinyUSB..."
-TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
+TINYUSB_REPO_URL="https://github.com/reykada/tinyusb.git"
 TINYUSB_REPO_DIR="$AR_COMPS/arduino_tinyusb/tinyusb"
 if [ ! -d "$TINYUSB_REPO_DIR" ]; then
     git clone "$TINYUSB_REPO_URL" "$TINYUSB_REPO_DIR"
